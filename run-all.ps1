0863af46adc2607c31a5fb5a5f6216de7037949b
David Burel <david.burel@burelovi.eu>
2025-05-27 16:13:48 +0200

    Add get-networktraffic.ps1 execution to run-all.ps1 for comprehensive data retrieval

--- run-all.ps1
+++ run-all.ps1
@@ -9,6 +9,8 @@ param (
 
 & ./Auditing/Resources/PowerShell/get-inventory.ps1 -CaseName $CaseName
 & ./Auditing/Resources/PowerShell/get-roleassignment.ps1 -CaseName $CaseName
+& ./Auditing/Resources/PowerShell/get-networktraffic.ps1 -CaseName $CaseName
+
 & ./Auditing/EntraID/PowerShell/get-entrausers.ps1 -CaseName $CaseName -AllDetails -InactiveDays 90
 & ./Auditing/EntraID/PowerShell/get-entragroups.ps1 -CaseName $CaseName
 & ./Auditing/EntraID/PowerShell/get-applications.ps1 -CaseName $CaseName
